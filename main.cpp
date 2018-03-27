e242549a241e800a8eb90ba12527e871a82b1546
karling93 <459607025@qq.com>
2018-03-27 16:53:23 +0800

    multi-file compilation

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "test.h"
+int main()
+{
+	test("hello world!");
+	return 0;
+}
---
+++ test_fn.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+#include "test.h"
+
+void test(const char* name)
+{
+	cout<<"hello world!"<<endl;
+}
